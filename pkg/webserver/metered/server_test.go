9f4c3ee5249475abc6269ee3d9c014f13a647b4a
Antony Ho <ntonyworkshop@gmail.com>
2019-06-12 00:20:47 +0800

    Add unit tests on metered web server
    - Found data races on store() and count() functions.

--- pkg/webserver/metered/server_test.go
+++ pkg/webserver/metered/server_test.go
@@ -2,7 +2,9 @@ package metered_test
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"testing"
 
 	"github.com/antonyho/go-project-demo/pkg/webserver/metered"
 )
@@ -17,3 +19,42 @@ func ExampleNewWebServer() {
 }
 
 // TODO - Unit test functions on unexported functions
+func TestNewWebServer(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(resp, "hello")
+	})
+	meteredWebServer := metered.NewWebServer(mux)
+	go meteredWebServer.Listen(":9080")
+
+	t.Run("GetRoot", func(t *testing.T) {
+		resp, err := http.Get("http://localhost:9080/")
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+		respBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+		if string(respBody) != "hello" {
+			t.Errorf("Unexpected HTTP response content: %s. Expecting: hello.", string(respBody))
+		}
+	})
+	t.Run("GetStat", func(t *testing.T) {
+		resp, err := http.Get("http://localhost:9080/stat")
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+		respBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+		if string(respBody) != "Total Request in Last 60 Seconds: 1" {
+			t.Errorf("Unexpected HTTP response content: %s. Expecting: Total Request in Last 60 Seconds: 1.", string(respBody))
+		}
+	})
+}
